dd3abd3104c8b007f893dc28be01a3463a33bb85
Zane Duffield <duffieldzane@gmail.com>
2023-12-14 00:02:40 +1100

    solve day 9

--- src/day_09/mod.rs
+++ src/day_09/mod.rs
@@ -1,13 +1,17 @@
 use itertools::Itertools;
-use nom::{bytes::complete::*, character::complete::*, multi::*, sequence::tuple, Parser};
+use nom::{
+    character::complete::{i64, line_ending, space1},
+    multi::{separated_list0, separated_list1},
+};
 
 pub struct Input {
-    lines: Vec<Vec<i64>>,
+    line_row_diffs: Vec<Vec<Vec<i64>>>,
 }
 
 fn parse_input(input: &str) -> nom::IResult<&str, Input> {
     let (input, lines) = separated_list0(line_ending, separated_list1(space1, i64))(input)?;
-    Ok((input, Input { lines }))
+    let line_row_diffs = lines.iter().map(|seq| line_row_diffs(seq)).collect();
+    Ok((input, Input { line_row_diffs }))
 }
 
 #[aoc_generator(day9)]
@@ -17,36 +21,49 @@ pub fn input_generator(input: &str) -> Input {
     result
 }
 
-fn seq_head_and_tail(seq: &[i64]) -> (i64, i64) {
-    let mut rows = vec![seq.to_vec()];
+fn line_row_diffs(seq: &[i64]) -> Vec<Vec<i64>> {
+    // we could probably store all these row differences in one flat vector for performance
+    // but that would be a pain and this is fast enough already
+    let mut row_diffs = vec![seq.to_vec()];
     loop {
-        rows.push(
-            rows.last()
+        row_diffs.push(
+            row_diffs
+                .last()
                 .unwrap()
                 .iter()
                 .tuple_windows()
                 .map(|(a, b)| b - a)
-                .collect::<Vec<_>>(),
+                .collect(),
         );
-        if rows.last().unwrap().iter().all(|i| *i == 0) {
+        if row_diffs.last().unwrap().iter().all(|i| *i == 0) {
             break;
         }
     }
-
-    let next = rows.iter().flat_map(|row| row.last()).sum();
-    let prev = rows.iter().rev().skip(1).fold(0, |acc, row| row[0] - acc);
-
-    (prev, next)
+    row_diffs
 }
 
 #[aoc(day9, part1)]
 pub fn part_1(input: &Input) -> i64 {
-    input.lines.iter().map(|s| seq_head_and_tail(s).1).sum()
+    input
+        .line_row_diffs
+        .iter()
+        .map(|row_diffs| row_diffs.iter().flat_map(|row| row.last()).sum::<i64>())
+        .sum()
 }
 
 #[aoc(day9, part2)]
 pub fn part_2(input: &Input) -> i64 {
-    input.lines.iter().map(|s| seq_head_and_tail(s).0).sum()
+    input
+        .line_row_diffs
+        .iter()
+        .map(|row_diffs| {
+            row_diffs
+                .iter()
+                .rev()
+                .skip(1)
+                .fold(0, |acc, row| row[0] - acc)
+        })
+        .sum()
 }
 
 #[cfg(test)]
